f57e31f3fb32a3feba9f12778a37ffe70e6d0f8d
Ethan Loflin <loflinethan@gmail.com>
2023-01-31 10:26:28 -0600

    Added comments for clarity, draw scorecard for one

---
+++ SWE.py
@@ -0,0 +1,19 @@
+import random
+
+
+dieOne = random.randint(1,7)
+
+if dieOne == 1:
+    print("One")
+elif dieOne == 2:
+    print("Two")
+elif dieOne == 3:
+    print("Three")
+elif dieOne == 4:
+    print("Four")
+elif dieOne == 5:
+    print("Five")
+elif dieOne == 6:
+    print("Six")
+elif dieOne == 7:
+    print("seven")
