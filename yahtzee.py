90ee30c00940f369c133950867783df3099412f8
Ethan Loflin <loflinethan@gmail.com>
2023-02-06 19:21:34 -0600

    Finished up scoring, added game reset function.

--- yahtzee.py
+++ yahtzee.py
@@ -18,65 +18,72 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 numbers_list = [0, 0, 0, 0, 0,]
 roll = False
-rolls_left = 50
+rolls_left = 3
 selected = [False, False, False, False, False]
-selected_choice = [True, False, False, False, False, False, False, False, False, False, False, False, False, False]
+selected_choice = [False, False, False, False, False, False, False, False, False, False, False, False, False, False]
 possible = [False, False, False, False, False, False, False, False, False, False, False, False, False]
 done = [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
-score = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+scores = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 totals = [0, 0, 0, 0, 0, 0, 0]
 clicked = -1
-current_score = 0
+bonus_time = False
 player = 1
+something_selected = False
+score = 0
+game_over = False
 
-def check_scores(selected_choice, numbers_list, possible, current_score):
+def check_scores(selected_choice, numbers_list, possible, score):
     active = 0
     for index in range(len(selected_choice)):
         if selected_choice[index]:
             active = index
     if active == 0:
-        current_score = numbers_list.count(1)
+        score = numbers_list.count(1)
     elif active == 1:
-        current_score = numbers_list.count(2)
+        score = numbers_list.count(2) * 2
     elif active == 2:
-        current_score = numbers_list.count(3)
+        score = numbers_list.count(3) * 3
     elif active == 3:
-        current_score = numbers_list.count(4)
+        score = numbers_list.count(4) * 4
     elif active == 4:
-        current_score = numbers_list.count(5)
+        score = numbers_list.count(5) * 5
     elif active == 5:
-        current_score = numbers_list.count(6)
+        score = numbers_list.count(6) * 6
     elif active == 6 or active == 7:
         if possible[active]:
-                current_score = sum(numbers_list)
+                score = sum(numbers_list)
         else:
-                current_score = 0
+                score = 0
     elif active == 8:
         if possible[active]:
-            current_score = 25
+            score = 25
         else:
-            current_score = 0
+            score = 0
     elif active == 9:
         if possible[active]:
-            current_score = 30
+            score = 30
         else:
-            current_score = 0
+            score = 0
     elif active == 10:
         if possible[active]:
-            current_score = 40
+            score = 40
         else:
-            current_score = 0
+            score = 0
     elif active == 11:
         if possible[active]:
-            current_score = 50
+            score = 50
         else:
-            current_score = 0
+            score = 0
     elif active == 12:
-        current_score = sum(numbers_list)
-    return current_score
+        score = sum(numbers_list)
+    return score
 
 #Draws scorecard outline + buttons
 def draw_stuff():
+    global game_over
+    if game_over:
+        over_text = font.render("Game Over: Click to Restart", True, white)
+        screen.blit(over_text, 280, 290)
     roll_text = font.render("Click To Roll", True, white)
     screen.blit(roll_text, (100, 167))
     accept_text = font.render("Accept Turn", True, white)
@@ -144,39 +151,31 @@ class Dice:
                 selected[self.key] = True
 ##Class for choice, draws table for choices and text for each choice.
 class Choice:
-    def __init__(self, x_pos, y_pos, text, select, possible, done, score):
-        global selected_choice
+    def __init__(self, x_pos, y_pos, text, select, possibles, dones, my_score):
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.select = select
         self.text = text
-        self.done = done
-        self.possible = possible
-        self.score = score
-        
+        self.selected = select
+        self.possible = possibles
+        self.done = dones
+        self.score = my_score
+
     def draw(self):
-        pygame.draw.line(screen, black, (self.x_pos, self.y_pos), (self.x_pos + 225, self.y_pos), 5)
-        pygame.draw.line(screen, black, (self.x_pos, self.y_pos + 30), (self.x_pos + 225, self.y_pos + 30), 5)
-        ##If statement that determines whether die rolled is available to keep
+        pygame.draw.line(screen, (0, 0, 0), (self.x_pos, self.y_pos + 31), (self.x_pos + 225, self.y_pos + 31), 2)
+        name_text = ''
         if not self.done:
             if self.possible:
-                my_text = font.render(self.text, True, (34, 140, 34))
+                name_text = font.render(self.text, True, (34, 140, 34))
             elif not self.possible:
-                my_text = font.render(self.text, True, (255, 0, 0))
+                name_text = font.render(self.text, True, (255, 0, 0))
         else:
-            my_text = font.render(self.text, True, black)
-        if self.select:
-            pygame.draw.rect(screen, (20, 35, 30), [self.x_pos, self.y_pos, 155, 30])
-        screen.blit(my_text, (self.x_pos + 10, self.y_pos + 5))
+            name_text = font.render(self.text, True, (0, 0, 0))
+        if self.selected:
+            pygame.draw.rect(screen, (20, 35, 30), [self.x_pos, self.y_pos + 2, 155, 30])
+        screen.blit(name_text, (self.x_pos + 5, self.y_pos + 10))
         score_text = font.render(str(self.score), True, (0, 0, 255))
         screen.blit(score_text, (self.x_pos + 165, self.y_pos + 10))
         
-    def check_click(self, coordinates):
-        if self.die.collidepoint(coordinates):
-            if selected[self.key]:
-                selected[self.key] = False
-        elif not selected[self.key]:
-            selected[self.key] = True
         
 #Score class is to draw score of PlayerScoreCard results
 class Score:
@@ -187,7 +186,38 @@ class Score:
 
     def draw(self):
         my_text = font.render(self.text, True, black)
-        
+
+
+
+def restart_function():
+    global roll
+    global numbers_list
+    global selected
+    global clicked
+    global rolls_left
+    global scores
+    global choice
+    global done
+    global possible
+    global totals
+    global something_selected
+    global score
+    roll = False
+    number_list = [7, 8, 9, 10, 11]
+    selected_choice = [False, False, False, False, False]
+    clicked = False
+    rolls_left = 3
+    scores = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    choice = [False, False, False, False, False, False, False, False, False, False, False, False, False]
+    done = [False, False, False, False, False, False, False, False, False, False, False, False, False]
+    possible = [False, False, False, False, False, False, False, False, False, False, False, False, False]
+    totals = [0, 0, 0, 0, 0, 0, 0]
+    something_selected = False
+    score = 0
+    
+
+
+       
 def check_possibilities(possible_list, numbers_list):
     ##How does it handle running out of possibliities?
     ##Checks to see if there are any dice that can be kept for each choice
@@ -261,13 +291,30 @@ def make_choice(clicked_num, selected, done_list):
     if not done_list[clicked_num]:
         selected[clicked_num] = True
     return selected
-      
+
+def check_totals(totals_list, scores_list, my_bonus):
+    totals_list[0] = scores_list[0] + scores_list[1] + scores_list[2] + scores_list[3] + scores_list[4] + scores_list[5]
+    if totals_list[0] >= 63:
+        totals_list[1] = 35
+    else:
+        totals_list[1] = 0
+    totals_list[2] = totals_list[0] + totals_list[1]
+
+    totals_list[4] = scores_list[6] + scores_list[7] + scores_list[8] + scores_list[9] + scores_list[10] + \
+        scores_list[11] + scores_list[12]
+    totals_list[5] = totals_list[2]
+    totals_list[6] = totals_list[4] + totals_list[5]
+    if my_bonus:
+        totals_list[3] += 100
+        my_bonus = False
+    return totals_list, my_bonus
 ##Main Loop
 running = True
 while running:
     timer.tick(fps)
     screen.fill(background)
-    
+    if game_over:
+        restart_button = pygame.draw.rect(screen, black, [275, 275, 280, 30])
     ##Draws 5 Die at top of screen
     die1 = Dice(10, 50, numbers_list[0], 0)
     die2 = Dice(130, 50, numbers_list[1], 1)
@@ -276,29 +323,31 @@ while running:
     die5 = Dice(490, 50, numbers_list[4], 4)
     
     
-    ones = Choice(0, 200, "Ones", selected_choice[0], possible[0], done[0], score[0])
-    twos = Choice(0, 230, "Twos", selected_choice[1], possible[1], done[1], score[1])
-    threes = Choice(0, 260, "Threes", selected_choice[2], possible[2], done[2], score[2])
-    fours = Choice(0, 290, "Fours", selected_choice[3], possible[3], done[3], score[3])
-    fives = Choice(0, 320, "Fives", selected_choice[4], possible[4], done[4], score[4])
-    sixes = Choice(0, 350, "Sixes", selected_choice[5], possible[5], done[5], score[5])
+    ones = Choice(0, 200, "Ones", selected_choice[0], possible[0], done[0], scores[0])
+    twos = Choice(0, 230, "Twos", selected_choice[1], possible[1], done[1], scores[1])
+    threes = Choice(0, 260, "Threes", selected_choice[2], possible[2], done[2], scores[2])
+    fours = Choice(0, 290, "Fours", selected_choice[3], possible[3], done[3], scores[3])
+    fives = Choice(0, 320, "Fives", selected_choice[4], possible[4], done[4], scores[4])
+    sixes = Choice(0, 350, "Sixes", selected_choice[5], possible[5], done[5], scores[5])
     upper_total1 = Choice(0, 380, "Total Score", False, False, True, totals[0])
     upper_bonus = Choice(0, 410, "Bonus if >= 63", False, False, True, totals[1])
     upper_total2 = Choice(0, 440, "Upper Total", False, False, True, totals[2])
-    three_kind = Choice(0, 470, "Three of a Kind",selected_choice[6], possible[6], done[6], score[6])
-    four_kind = Choice(0, 500, "Four of a Kind", selected_choice[7], possible[7], done[7],  score[7])
-    full_house = Choice(0, 530, "Full House", selected_choice[8], possible[8], done[8], score[8])
-    small_straight = Choice(0, 560, "Small Straight",selected_choice[9], possible[9], done[9], score[9])
-    large_straight = Choice(0, 590, "Large Straight", selected_choice[10], possible[10], done[10], score[10])
-    yahtzee = Choice(0, 620, "Yahtzee",selected_choice[11], possible[11], done[11], score[11])
-    chance = Choice(0, 650, "Chance", selected_choice[12], possible[12], done[12], score[12])
+    three_kind = Choice(0, 470, "Three of a Kind",selected_choice[6], possible[6], done[6], scores[6])
+    four_kind = Choice(0, 500, "Four of a Kind", selected_choice[7], possible[7], done[7],  scores[7])
+    full_house = Choice(0, 530, "Full House", selected_choice[8], possible[8], done[8], scores[8])
+    small_straight = Choice(0, 560, "Small Straight",selected_choice[9], possible[9], done[9], scores[9])
+    large_straight = Choice(0, 590, "Large Straight", selected_choice[10], possible[10], done[10], scores[10])
+    yahtzee = Choice(0, 620, "Yahtzee",selected_choice[11], possible[11], done[11], scores[11])
+    chance = Choice(0, 650, "Chance", selected_choice[12], possible[12], done[12], scores[12])
     yahtzee_bonus = Choice(0, 680, "Yahtzee Bonus", False, False, True, totals[3])
     lower_total = Choice(0, 710, "Lower Total", False, False, True, totals[4])
     lower_total2 = Choice(0, 740, "Upper Total", False, False, True, totals[5])
     grand_total = Choice(0, 770, "Grand Total", False, False, True, totals[6])
     possible = check_possibilities(possible, numbers_list)
-    current_score = check_scores(selected_choice, numbers_list, possible, score)
-    
+    score = check_scores(selected_choice, numbers_list, possible, scores)
+    totals, bonus_time = check_totals(totals, scores, bonus_time)
+    if True in selected_choice:
+        something_selected = True
     
     roll_button = pygame.draw.rect(screen, black, [10, 160, 280, 30])
     accept_button = pygame.draw.rect(screen, black, [310, 160, 280, 30])
@@ -339,11 +388,31 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
+            if game_over and restart_button.collidepoints(event.pos):
+                restart_function()
+                game_over = False
             die1.check_click(event.pos)
             die2.check_click(event.pos)
             die3.check_click(event.pos)
             die4.check_click(event.pos)
             die5.check_click(event.pos)
+            if roll_button.collidepoint(event.pos) and rolls_left > 0:
+                roll = True
+                rolls_left -= 1
+                draw_clicked = 0
+            if accept_button.collidepoint(event.pos) and something_selected and rolls_left < 3:
+                if scores[11] == 50 and done[11] and possible[11]:
+                    bonus_time = True
+                for i in range(len(selected_choice)):
+                    if selected_choice[i]:
+                        done[i] = True
+                        scores[i] = score
+                        selected_choice[i] = False
+                for i in range(len(selected)):
+                    selected[i] = False
+                rolls_left = 3
+                numbers_list = [7, 8, 9, 10, 11]
+                something_selected = False
             if 0 <= event.pos[0] <= 155:
                 if 200 <= event.pos[1] <= 380  or 470 <= event.pos[1] <= 680:
                     if 200 < event.pos[1] < 230:
@@ -374,10 +443,8 @@ while running:
                         clicked = 12
                     selected_choice = make_choice(clicked, selected_choice, done)
 
-        ##Button rolls dice if clicked
-            if roll_button.collidepoint(event.pos) and rolls_left > 0:
-                roll = True
-                rolls_left -= 1
+ 
+                    
                 
     ##Resets roll to false
     if roll:
